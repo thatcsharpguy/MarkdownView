784a06cd54e8a9270b2a7fd87856826d24764fca
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-11-22 20:45:37 -0600

    Added test app.

---
+++ ViewMarkdown.Test/Core/ViewMarkdown.TestPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ViewMarkdown.Test
+{
+	public partial class ViewMarkdown.TestPage : ContentPage
+	{
+		public ViewMarkdown.TestPage ()
+		{
+			InitializeComponent();
+}
+	}
+}
