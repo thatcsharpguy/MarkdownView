a342f71f8bcefad4ebe1b38f002aed5ddd15a473
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-05 17:57:14 -0600

    Added nuget test project

---
+++ ViewMarkdown.NuGetTest/WinPhone/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace ViewMarkdown.Test.WinPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage 
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            LoadApplication(new ViewMarkdown.Test.App());
+        }
+    }
+}
